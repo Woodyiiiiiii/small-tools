675beaf62a9267a8194f48b005ccdcdb6056f24c
woodyiiiiiii <wandi.mo@qq.com>
2021-12-01 22:04:06 +0800

    first update

---
+++ src/main/java/com/example/smallTools/SmallToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smallTools/SmallToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smallTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
