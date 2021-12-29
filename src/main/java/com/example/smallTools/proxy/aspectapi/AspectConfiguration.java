ae1786171e1093a0dca9db74b43ceee60f8dd1c7
woodyiiiiiii <wandi.mo@qq.com>
2021-12-29 22:55:00 +0800

    Spring Aspect Api

---
+++ src/main/java/com/example/smallTools/proxy/aspectapi/AspectConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.smallTools.proxy.aspectapi;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * @Author: woodyiiiiiii
+ * @Date: 2021/12/29 22:37
+ */
+@Aspect
+public class AspectConfiguration {
+}
---
+++ src/main/java/com/example/smallTools/proxy/aspectapi/AspectJApiTester.java
@@ -0,0 +1,42 @@
+package com.example.smallTools.proxy.aspectapi;
+
+import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description: Spring中的AspectJ代理
+ * @Author: woodyiiiiiii
+ * @Date: 2021/12/29 22:31
+ */
+public class AspectJApiTester {
+
+    public static void main(String[] args) {
+
+        Map<String, Object> map = new HashMap<>();
+
+        // create a proxy factory
+        AspectJProxyFactory factory = new AspectJProxyFactory(map);
+
+        // add the aspect
+        factory.addAspect(AspectConfiguration.class);
+
+        // add advice
+        factory.addAdvice((MethodBeforeAdvice) (method, args1, target) -> {
+            if (method.getName().equals("put") && args1.length == 2) {
+                System.out.println("before put");
+                System.out.printf("key: %s, value: %s\n", args1[0], args1[1]);
+            }
+        });
+
+        // create the proxy
+        Map<String, Object> proxy = factory.getProxy();
+
+        // use the proxy
+        proxy.put("key", "value");
+
+    }
+
+}
